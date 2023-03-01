ac43514d39bca7b46c0c7f6bceb26d11b7a9b186
Samantha Campisi <samantha.campisi@drinksandco.com>
2023-03-01 13:20:27 +0100

    Add carousel component to display gallery in HotelDetailScreen

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,82 @@
+import React, { useRef, useState } from "react";
+import {
+  Image,
+  View,
+  StyleProp,
+  ImageStyle,
+  ViewStyle,
+  ImageResizeMode,
+} from "react-native";
+import PagerView, { PagerViewOnPageScrollEvent } from "react-native-pager-view";
+
+import styles from "./Carousel.style";
+
+const Carousel = ({
+  gallery,
+  testID,
+  imageStyle,
+  imageResize,
+  indicatorsStyle,
+}: CarouselProps) => {
+  const [indexPage, setIndexPage] = useState(0);
+
+  const viewPagerRef = useRef<PagerView>(null);
+
+  const RenderIndicators = (id?: number) => {
+    return (
+      <View
+        key={id}
+        style={[styles.dot, id === indexPage ? styles.activeDot : null]}
+        testID={`${testID}-dot-${id}`}
+      />
+    );
+  };
+
+  return (
+    <View testID={testID} style={styles.container}>
+      {gallery.length && (
+        <View
+          style={[styles.indicatorsContainer, indicatorsStyle]}
+          testID={`${testID}-dots`}
+        >
+          {gallery.map((_, index: number) => {
+            return RenderIndicators(index);
+          })}
+        </View>
+      )}
+
+      <PagerView
+        ref={viewPagerRef}
+        style={{ flex: 1 }}
+        initialPage={0}
+        showPageIndicator={false}
+        overdrag={true}
+        onPageScroll={(event: PagerViewOnPageScrollEvent) => {
+          setIndexPage(event?.nativeEvent?.position);
+        }}
+      >
+        {gallery.map((item: string, index: number) => {
+          return (
+            <View key={index} testID={`${testID}-item-${index}`}>
+              <Image
+                source={{ uri: item }}
+                style={[styles.image, imageStyle]}
+                resizeMode={imageResize || "cover"}
+              />
+            </View>
+          );
+        })}
+      </PagerView>
+    </View>
+  );
+};
+
+export interface CarouselProps {
+  gallery: string[];
+  testID?: string;
+  imageStyle?: StyleProp<ImageStyle>;
+  imageResize?: ImageResizeMode;
+  indicatorsStyle?: StyleProp<ViewStyle>;
+}
+
+export default Carousel;
--- src/screens/HotelDetailScreen/HotelDetailScreen.tsx
+++ src/screens/HotelDetailScreen/HotelDetailScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import Carousel from "../../components/Carousel";
 import InfoLine from "../../components/InfoLine";
 import Stars from "../../components/Stars";
 
@@ -20,26 +21,28 @@ const HotelDetailScreen = ({ route }) => {
 
   return (
     <View testID={"HotelDetailScreen"}>
-      <Text>{name}</Text>
-      <Stars value={stars} />
-      <InfoLine
-        label={`${location.address}, ${location.city}`}
-        icon={require("../../../assets/icons/location.png")}
-      />
-      <InfoLine
-        label={String(userRating)}
-        icon={require("../../../assets/icons/rating.png")}
-      />
-      <InfoLine
-        label={"Check-in / Check-out"}
-        icon={require("../../../assets/icons/clock.png")}
-      />
-      <InfoLine
-        label={`${checkIn.from}-${checkIn.to} / ${checkOut.from}-${checkOut.to}`}
-        icon={require("../../../assets/icons/clock.png")}
-      />
+      <Carousel gallery={gallery} />
+      <View>
+        <Stars value={stars} />
+        <InfoLine
+          label={String(userRating)}
+          icon={require("../../../assets/icons/rating.png")}
+        />
+        <InfoLine
+          label={"Check-in / Check-out"}
+          icon={require("../../../assets/icons/clock.png")}
+        />
+        <InfoLine
+          label={`${checkIn.from}-${checkIn.to} / ${checkOut.from}-${checkOut.to}`}
+          icon={require("../../../assets/icons/clock.png")}
+        />
+        <InfoLine
+          label={`${location.address}, ${location.city}`}
+          icon={require("../../../assets/icons/location.png")}
+        />
+      </View>
       <MapView
-        style={{ width: "100%", height: 250 }}
+        style={{ width: "100%", height: 240, marginVertical: 16 }}
         initialRegion={{
           latitude: location.latitude,
           longitude: location.longitude,
