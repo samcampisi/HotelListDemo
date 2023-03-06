8dfe642df99fec4b401d1d2e26783518b9e67704
Samantha Campisi <samantha.campisi@drinksandco.com>
2023-03-06 12:43:39 +0100

    Add babel module resolver and prettier config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            assets: './assets',
+            mocks: './__mocks__',
+            contexts: './src/contexts',
+            components: './src/components',
+            config: './src/config',
+            constants: './src/constants',
+            hooks: './src/hooks',
+            interfaces: './src/interfaces',
+            screens: './src/screens',
+            theme: './src/theme',
+            utils: './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
